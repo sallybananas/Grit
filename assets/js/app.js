b4c0d1c57b4b2a5904e62c6ed61e8957958ee2d2
sallybananas <saljgiani@gmail.com>
2017-11-03 23:32:24 -0600

    bunch of little updates

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// // Select all tabs
+// $('#fixtab').click(function(){
+//     console.log("how button clicked");
+//     $(this).tab('show');
+// })
+
+// // Select tab by name
+// $('.nav-tabs a[href="#how"]').tab('show')
