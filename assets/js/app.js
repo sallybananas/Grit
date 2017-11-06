089f7dbbef1025499f3875b1d724608d83cc9125
sallybananas <saljgiani@gmail.com>
2017-11-06 13:20:21 -0700

    form updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,10 @@
 
 // // Select tab by name
 // $('.nav-tabs a[href="#how"]').tab('show')
+$('select').change(function() {
+    if ($(this).children('option:first-child').is(':selected')) {
+      $(this).addClass('placeholder');
+    } else {
+     $(this).removeClass('placeholder');
+    }
+   });
