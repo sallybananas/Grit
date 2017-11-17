f78e60ae7f4316e226b6909d51c145424e64e77d
sallybananas <saljgiani@gmail.com>
2017-11-16 18:37:57 -0700

    linking firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,36 +1,31 @@
-$(function(){
-    //make a variable to keep track of data coming from firebase
-
  // Initialize Firebase
  var config = {
-    apiKey: "AIzaSyBePGAjREPI1LZUsZGc9B9f4KF-yYMDe3A",
-    authDomain: "youth-grit-scale.firebaseapp.com",
-    databaseURL: "https://youth-grit-scale.firebaseio.com",
-    projectId: "youth-grit-scale",
-    storageBucket: "",
-    messagingSenderId: "481575498972"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBePGAjREPI1LZUsZGc9B9f4KF-yYMDe3A",
+  authDomain: "youth-grit-scale.firebaseapp.com",
+  databaseURL: "https://youth-grit-scale.firebaseio.com",
+  projectId: "youth-grit-scale",
+  storageBucket: "youth-grit-scale.appspot.com",
+  messagingSenderId: "481575498972"
+};
+firebase.initializeApp(config);
 
+    var database = firebase.database();
     var data= [];
+    var genderToSend = "";
+    var selectAge = "";
+    var birthOrderToSend = "";
+    var zipToSend = "";
+    var educationToSend = "";
 
-    //create new connection to firebase
-      var ref= new Firebase('https://data-application1.firebaseio.com/activities');
-
-    //listen to data updates from firebase
-      ref.on("value", function (snapshot){
-      console.log(snapshot.val());
-     //when the data updates at firebase, put it in the data variable
-      data= snapshot.val();
-    })
   //Entire Form (handler)
-  $('#newActivity').submit(function(event) {
+  $('#submit-grit-score').on('click', function(event){
+    event.preventDefault();
 
-    var $form = $(this);
-    console.log("Submit to Firebase");
+    // var $form = $(this);
+    // console.log("Submit to Firebase");
 
-    //disable submit button
-    $form.find("#saveForm").prop('disabled', true);
+    // //disable submit button
+    // $form.find("#saveForm").prop('disabled', true);
 
     //get values to send to Firebase
     var genderToSend = $('#gender').val();
@@ -39,34 +34,32 @@ $(function(){
     var selectAge = $('#selectAge').val();
     console.log(selectAge);
 
-    var birthOrderToSend= $('#bo').val();
+    var birthOrderToSend = $('#bo').val();
     console.log(birthOrderToSend);
 
-    var zipToSend= $('#zip').val();
+    var zipToSend = $('#zip').val();
     console.log(zipToSend);
 
-    var educationToSend= $('#education').val();
+    var educationToSend = $('#education').val();
     console.log(educationToSend);
 
     //take the values from the form, and put them in an object
-    var newActivity= {
-      "gender": genderToSend,
-      "selectAge": selectAge,
-      "bo": birthOrderToSend,
-      "zip": zipToSend,
-      "education": educationToSend
-    }
+    database.ref("/youth").push({
+      gender: genderToSend,
+      selectage: selectAge,
+      bo: birthOrderToSend,
+      zip: zipToSend,
+      education: educationToSend
+    });
     //put new object in data array
-    data.push(newActivity);
-    console.log(data);
+    // data.push(newActivity);
+    // console.log(data);
 
       //send the new data to Firebase
-          ref.set(data, function(err){
-        if(err){
-          alert("Data no go");
-        }
-      });
+      //     ref.set(data, function(err){
+      //   if(err){
+      //     alert("Data no go");
+      //   }
+      // });
 
-      return false;
-    })
-})
+  })
